38f352ed841218290aae201e914e259801e2a5fa
m4rkbello <markbello10041999@gmail.com>
2024-12-31 03:12:34 +0800

    ADDED

--- App.tsx
+++ App.tsx
@@ -1,23 +1,86 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 import React from 'react';
+import type {PropsWithChildren} from 'react';
 import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
   StyleSheet,
   Text,
+  useColorScheme,
   View,
 } from 'react-native';
-import { Header, Colors, ReloadInstructions, DebugInstructions, LearnMoreLinks } from 'react-native/Libraries/NewAppScreen';
-
 
 
 function App(): React.JSX.Element {
-
   return (
-    <View>
+    <View style={styles.container}>
 
+      <View style={styles.boxContainer} >
+
+      <View style={styles.redBoxContainer} />
+      <View style={styles.blueBoxContainer} />
+      <View style={styles.greenBoxContainer} />
+      </View>
+      <Text style={styles.nestedText}>
+        MY FIRST REACT NATIVE COURSE 
+        <Text style={styles.innerText}>
+          SIGE PADAYON
+        </Text>
+      </Text>
+      <Image style={styles.image} source={{uri: 'https://i.ibb.co/F7df0bb/pic-removebg-preview.png'}} />
+
+      <Image so />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+    container: {
+      marginBottom: 20,
+    },
+
+    boxContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    },
+
+    redBoxContainer: {
+      width: 50,
+      height: 50,
+      backgroundColor: 'red',
+    },
+    blueBoxContainer: {
+      width: 50,
+      height: 50,
+      backgroundColor: 'blue',
+    },
+    greenBoxContainer: {
+      width: 50,
+      height: 50,
+      backgroundColor: 'green',
+    },
+    nestedText: {
+      fontSize: 30
+    },
+    innerText: {
+      fontSize: 30,
+      fontWeight: 'bold'
+    },
+    image: {
+      resizeMode: 'contain',
+      height: 150,
+      width: 150,
+      marginBottom:  10
+    }
+
 
 });
 
